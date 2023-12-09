84f888512f598903a7c2636df09bfabbde86900b
Kishna Bansal <kb.20051981@gmail.com>
2023-12-09 23:06:16 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+backgroundColor: Color.fromARGB(255, 64, 255, 195),
+        body: Center(
+          child: Text("Hello"),
+        ),
+      ),
+    ),
+  );
+}
